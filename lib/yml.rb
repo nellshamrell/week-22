9676c559b56d84899214ed5a67592dae861c8e46
John Chaffee <johnchaffee@mac.com>
2012-04-17 21:50:43 -0700

    Delete unused file

--- lib/yml.rb
+++
@@ -1,9 +0,0 @@
-require 'YAML'
-
-configuration_root_dir = File.join(File.dirname(__FILE__),"..","fixtures")
-
-file_contents = File.read "#{configuration_root_dir}/configuration.yml"
-
-configuration_data = YAML::load file_contents
-
-puts configuration_data
