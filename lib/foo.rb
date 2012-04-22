196e589ecb4ee133cfd2bec90f6515126a8fbd72
Nell Shamrell <nshamrell@bluebox.net>
2012-04-22 14:35:59 -0700

    work in progress, saving before the plane lands

--- lib/foo.rb
+++ lib/foo.rb
@@ -1,46 +1,51 @@
 require_relative 'my_xml'
 require_relative 'my_yml'
 
-@true = []
-@false = []
+class Foo
 
-def load_my_file(filename)
-  configuration_root_dir = File.join(File.dirname(__FILE__),"..","fixtures")
-  begin
-    @file_contents = File.read "#{configuration_root_dir}/#{filename}"
-  rescue Errno::ENOENT => error
-    warn "WARNING: #{error.class} - #{error}"
-    @file_contents = nil
+  def self.load_my_file(filename)
+    configuration_root_dir = File.join(File.dirname(__FILE__),"..","fixtures")
+    begin
+      @file_contents = File.read "#{configuration_root_dir}/#{filename}"
+    rescue Errno::ENOENT => error
+      warn "WARNING: #{error.class} - #{error}"
+      @file_contents = nil
+    end
   end
-end
 
-def parse_my_file(file)
-  if file =~ /.*\.xml$/
-    parse_my_xml
-  elsif file =~ /.*\.yml$/
-    parse_my_yml
+  def self.parse_my_file(file)
+    if file =~ /.*\.xml$/
+      x = MyXml.new
+      x.parse_my_xml(@file_contents)
+    elsif file =~ /.*\.yml$/
+      parse_my_yml
+    end
   end
-end
 
-def build_my_lists(subscribe,email)
-  subscribe == true ? @true << email : @false << email
-end
+  def build_my_lists(subscribe,email)
 
-def merge_lists
-  @true = @true.sort.uniq.compact
-  puts "@true: #{@true}"
-  @false = @false.sort.uniq.compact
-  puts "@false: #{@false}"
-  @emails = @true - @false  # Subtract false array from true array
-  puts "@emails: #{@emails}"
-end
+    @true = []
+    @false = []
 
-# Pass files through command line e.g. "ruby foo.xml foo.yml foo.doc"
-files = ARGV
-puts "ARGV: #{ARGV}"
-files.each do |file|
-  puts "file: #{file}"
-  load_my_file(file)
-  parse_my_file(file)
+    subscribe == true ? @true << email : @false << email
+  end
+
+  def self.merge_lists
+    @true = @true.sort.uniq.compact
+    puts "@true: #{@true}"
+    @false = @false.sort.uniq.compact
+    puts "@false: #{@false}"
+    @emails = @true - @false  # Subtract false array from true array
+    puts "@emails: #{@emails}"
+  end
+
+  # Pass files through command line e.g. "ruby foo.xml foo.yml foo.doc"
+  files = ARGV
+  puts "ARGV: #{ARGV}"
+  files.each do |file|
+    puts "file: #{file}"
+    load_my_file(file)
+    parse_my_file(file)
+  end
+  merge_lists
 end
-merge_lists
--- lib/my_xml.rb
+++ lib/my_xml.rb
@@ -1,13 +1,24 @@
 require 'nokogiri'
+class MyXml
 
-def parse_my_xml
-  configuration_data = Nokogiri::XML(@file_contents).root
-  configuration_data.xpath('environment').each do |environment|
-    puts environment.xpath('@name')
-    puts environment.xpath('email').text
-    puts environment.xpath('subscribe').text
-    email = environment.xpath('email').text
-    subscribe = environment.xpath('subscribe').text
-    build_my_lists(subscribe,email)
+  def load_config(file_contents)
+    configuration_data = Nokogiri::XML(file_contents).root
+  end
+
+  def parse_my_xml(file_contents)
+    subscribers = []
+    configuration_data = load_config(file_contents)
+    configuration_data.xpath('environment').each do |environment|
+=begin
+      puts environment.xpath('@name')
+      puts environment.xpath('email').text
+      puts environment.xpath('subscribe').text
+=end
+      email = environment.xpath('email').text
+      subscribe = environment.xpath('subscribe').text
+      subscribers << {:email => email, :subscribe => subscribe}
+
+      return subscribers
+    end
   end
 end
